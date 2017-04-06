6a8eb131ae08694477fe77e8dd69b4e75fe1502e
garfielder <lyqqing@126.com>
2017-04-06 19:54:02 +0800

    Adding new test

--- MyTests/Palindrome/Source.cpp
+++ MyTests/Palindrome/Source.cpp
@@ -1,3 +1,6 @@
+/*
+* https://leetcode.com/problems/longest-palindromic-substring/#/description
+*/
 #include <iostream>
 #include <fstream>
 #include <map>
@@ -6,14 +9,58 @@
 #include <sstream>
 #include <cassert>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
 class Solution {
 public:
+	inline bool ValidIdx(const string& s, int idx) {
+		return  (idx >= 0) && (idx < s.size());
+	}
+
 	string longestPalindrome(string s) {
 
-		return "";
+		if (s.size() == 0) return "";
+
+		int i(0);
+		pair<int, int> maxIdx(0, 1);  // <start, size>
+		while (ValidIdx(s, i)) {
+			////
+			// Check pattern 'aba'
+			if (ValidIdx(s, i - 2) && (s[i - 2] == s[i])) {  // pattern hit
+				int m = i - 1;  // middle point
+				int j(1);
+				while (ValidIdx(s, m - j) && ValidIdx(s, m + j) && s[m - j] == s[m + j]) j++;
+
+				if (j > 1) j--;
+						
+				if (maxIdx.second  < 2 * j + 1) {
+					maxIdx.first = m - j;
+					maxIdx.second = 2 * j + 1;
+				}
+			}
+
+			// Check pattern 'aa'
+			if (ValidIdx(s, i - 1) && (s[i - 1] == s[i])) { // pattern hit
+				// m, n are middle elements pair
+				int m = i - 1;  
+				int n = i;
+				int j(0);
+				while (ValidIdx(s, m - j) && ValidIdx(s, n + j) && (s[m - j] == s[n + j])) j++;
+				if (j > 0) j--;
+
+				if (maxIdx.second < 2 * j + 2) {
+					maxIdx.first = m - j;
+					maxIdx.second = 2 * j + 2;
+				}
+					
+
+			}
+
+			i++;
+		}
+		return s.substr(maxIdx.first, maxIdx.second);
 	}
 };
 
@@ -29,7 +76,7 @@ void TestInput(string fname)
 		std::string line;
 		while (std::getline(ifs, line))
 		{
-			if ("" == line) {
+			if ("---" == line) {
 				cout << "New Test case " << endl;
 				string first;
 				getline(ifs, first);
